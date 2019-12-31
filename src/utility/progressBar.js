7a221fa376488f2d32610f7ff5dbd8ae98f786a1
Madhvi Kokil <madhvi.kokil@cuelogic.com>
2019-12-31 13:19:06 +0530

    eslint changes

--- src/utility/progressBar.js
+++ src/utility/progressBar.js
@@ -5,7 +5,7 @@ export default {
     displayProgressBar : (props) => {
         return (<td key={props.aValue}><span className="underLine-name">{props.aValue}</span>&nbsp;&nbsp;
               <div class="progress">
-                     <div class="inside underLine-name" style={{width:`${props.bar}` +   'px',backgroundColor:`${props.colors}`}}></div>  &nbsp;&nbsp;                
+                     <div class="inside underLine-name" style={{width:`${props.bar}px` ,backgroundColor:`${props.colors}`}}></div>  &nbsp;&nbsp;                
                       </div>&nbsp;&nbsp;{props.bar+'%'}
                     </td>)
 
